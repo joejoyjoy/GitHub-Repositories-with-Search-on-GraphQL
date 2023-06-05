44965a4e7817cc53342c5a800d7d18993f9a8cc5
Joe Alt <joejoyalt@gmail.com>
2023-06-05 17:36:50 +0200

    Fix: deploy bug fixed

--- src/views/pages/errorPage/errorPage.tsx
+++ src/views/pages/errorPage/errorPage.tsx
@@ -1,17 +1,7 @@
-import './errorPage.scss'
-import { Link } from 'react-router-dom'
+import ErrorPageComponent from "../../components/errorPageComponent/ErrorPageComponent"
 
 const ErrorPage = () => {
-  return (
-    <main className="error-page">
-      <h2>Oops!</h2>
-      <div>
-        <h3>404 - Page not found</h3>
-        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-        <Link to={'./dashboard'}>Go to homepage</Link>
-      </div>
-    </main>
-  )
+  return <ErrorPageComponent />
 }
 
 export default ErrorPage
