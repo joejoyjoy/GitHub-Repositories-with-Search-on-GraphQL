9dbcc0722c6e580faee8e81d0ca57d6c6c6b06a1
Joe Alt <joejoyalt@gmail.com>
2023-06-07 20:59:22 +0200

    Fix: estimated duration increased

--- src/views/components/loginLoader/LoginLoader.tsx
+++ src/views/components/loginLoader/LoginLoader.tsx
@@ -3,11 +3,16 @@ import Loader from "../../../UI/loader/Loader";
 import './loginLoader.scss'
 
 const LoginLoader = () => {
-  const [counter, setCounter] = useState(25);
+  const [counter, setCounter] = useState(100);
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(40);
 
   useEffect(() => {
-    const timer: any =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    const timer: any = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+
+    setMinutes(Math.floor(counter / 60))
+    setSeconds(counter % 60)
+
     return () => clearInterval(timer);
   }, [counter]);
 
@@ -16,7 +21,7 @@ const LoginLoader = () => {
       <div className="login-loader-component__wrapper">
         <Loader />
         <p>Please wait for the server to response</p>
-        <p>{counter !== 0 ? `Estimated time ${counter}s` : "Please refresh the page"}</p>
+        <p>{counter !== 0 ? `Estimated time${minutes !== 0 ? ` ${minutes} minute and` : ""} ${seconds} seconds` : "Please refresh the page"}</p>
       </div>
     </div>
   )
